d04d8fe2434144edf1308d8bb46a4899f83089f1
yutao <yutao@kdxfilm.com>
2017-11-28 16:12:16 +0800

    横竖屏正交投影

--- app/src/main/java/com/opengl/android/airhockey/AirHockeyRenderer.java
+++ app/src/main/java/com/opengl/android/airhockey/AirHockeyRenderer.java
@@ -3,6 +3,7 @@ package com.opengl.android.airhockey;
 import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import com.opengl.android.airhockey.util.LoggerConfig;
 import com.opengl.android.airhockey.util.ShaderHelper;
@@ -22,17 +23,20 @@ import javax.microedition.khronos.opengles.GL10;
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private static final int POSITION_COMPONENT_COUNT = 2;
     private static final int BYTES_PER_FLOAT = 4;
-    private static final String A_COLOR = "a_Color";
     private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int STRIDE =
             (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
-    private int aColorLocation;
+    private static final String U_MATRIX = "u_Matrix";
     private static final String A_POSITION = "a_Position";
+    private static final String A_COLOR = "a_Color";
     private int aPositionLocation;
-
+    private int aColorLocation;
     private final Context context;
     private final FloatBuffer vertexData;
     private int program;
+    private final float[] projectionMatrix = new float[16];
+    private int uMatrixLocation;
+
 
     public AirHockeyRenderer(Context context) {
         this.context = context;
@@ -40,19 +44,19 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
                 // Order of coordinates X, Y, R, G, B
                 // triangle fan
                 0f,     0f,     1f,   1f,   1f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f,  -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f,   0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f,  -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f,   0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
 
                 // line 1
                 -0.5f,    0f,   1f,   0f,   0f,
                 0.5f,     0f,   1f,   0f,   0f,
 
                 // Mallets
-                0f,  -0.25f,   0f,   0f,   1f,
-                0f,   0.25f,   1f,   0f,   0f
+                0f,  -0.4f,   0f,   0f,   1f,
+                0f,   0.4f,   1f,   0f,   0f
         };
         vertexData = ByteBuffer.allocateDirect(tableVertexWithTriangles.length * BYTES_PER_FLOAT)
                 .order(ByteOrder.nativeOrder())
@@ -75,6 +79,7 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         GLES20.glUseProgram(program);
         aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
         aColorLocation = GLES20.glGetAttribLocation(program, A_COLOR);
+        uMatrixLocation = GLES20.glGetUniformLocation(program, U_MATRIX);
 
         vertexData.position(0);
         GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT, false, STRIDE, vertexData);
@@ -88,11 +93,20 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         gl.glViewport(0, 0, width, height);
+        final float aspectRatio = (width > height) ? ((float) width / (float) height) : ((float) height / (float) width);
+        if(width > height) {
+            // landscape
+            Matrix.orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+        } else {
+            // Portrait or Square
+            Matrix.orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
         // draw table
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6);
         // draw lines
