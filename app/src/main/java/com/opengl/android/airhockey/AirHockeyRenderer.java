c8065075717c885ffa55ec2f598818d5a4e3e79d
yutao <yutao@kdxfilm.com>
2017-11-28 13:00:47 +0800

    FIXME:为什么画出来的没数据

--- app/src/main/java/com/opengl/android/airhockey/AirHockeyRenderer.java
+++ app/src/main/java/com/opengl/android/airhockey/AirHockeyRenderer.java
@@ -20,11 +20,16 @@ import javax.microedition.khronos.opengles.GL10;
  */
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
+    private static final String U_COLOR = "u_Color";
+    private int uColorLocation;
+    private static final String A_POSITION = "a_Position";
+    private int aPositionLocation;
     private static final int POSITION_COMPONENT_COUNT = 2;
     private static final int BYTES_PER_FLOAT = 4;
     private final Context context;
     private final FloatBuffer vertexData;
     private int program;
+
     public AirHockeyRenderer(Context context) {
         this.context = context;
         float[] tableVertexWithTriangles = {
@@ -51,7 +56,7 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     }
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        gl.glClearColor(0.4f, 0.0f, 0.0f, 0.0f);
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         String vertexShaderSource = TextResourceReader.readTextFileFromResource(context,
                 R.raw.simple_vertex_shader);
         String fragShaderSource = TextResourceReader.readTextFileFromResource(context,
@@ -62,6 +67,11 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         if(LoggerConfig.LOG_ON) {
             ShaderHelper.validateProgram(program);
         }
+        uColorLocation = GLES20.glGetUniformLocation(program, U_COLOR);
+        aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
+        vertexData.position(0);
+        GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT, false, 0, vertexData);
+        GLES20.glEnableVertexAttribArray(aPositionLocation);
     }
 
     @Override
@@ -72,5 +82,16 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         gl.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
+
+        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2);
+
+        GLES20.glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 1);
+
+        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 9, 1);
     }
 }
