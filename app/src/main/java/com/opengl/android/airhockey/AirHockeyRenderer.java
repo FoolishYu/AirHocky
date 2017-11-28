2ac4c717c31d161caf3ebb94721d289fc8b58a9c
yutao <yutao@kdxfilm.com>
2017-11-28 15:20:55 +0800

    使用颜色属性、三角形扇

--- app/src/main/java/com/opengl/android/airhockey/AirHockeyRenderer.java
+++ app/src/main/java/com/opengl/android/airhockey/AirHockeyRenderer.java
@@ -20,12 +20,16 @@ import javax.microedition.khronos.opengles.GL10;
  */
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
-    private static final String U_COLOR = "u_Color";
-    private int uColorLocation;
-    private static final String A_POSITION = "a_Position";
-    private int aPositionLocation;
     private static final int POSITION_COMPONENT_COUNT = 2;
     private static final int BYTES_PER_FLOAT = 4;
+    private static final String A_COLOR = "a_Color";
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int STRIDE =
+            (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+    private int aColorLocation;
+    private static final String A_POSITION = "a_Position";
+    private int aPositionLocation;
+
     private final Context context;
     private final FloatBuffer vertexData;
     private int program;
@@ -33,22 +37,22 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     public AirHockeyRenderer(Context context) {
         this.context = context;
         float[] tableVertexWithTriangles = {
-                // triangle 1
-                -0.5f, -0.5f,
-                0.5f, 0.5f,
-                -0.5f, 0.5f,
-                // triangle 2
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                0.5f, 0.5f,
+                // Order of coordinates X, Y, R, G, B
+                // triangle fan
+                0f,     0f,     1f,   1f,   1f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f,  -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f,   0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
 
                 // line 1
-                -0.5f, 0f,
-                0.5f, 0f,
+                -0.5f,    0f,   1f,   0f,   0f,
+                0.5f,     0f,   1f,   0f,   0f,
 
                 // Mallets
-                0f, -0.25f,
-                0f, 0.25f
+                0f,  -0.25f,   0f,   0f,   1f,
+                0f,   0.25f,   1f,   0f,   0f
         };
         vertexData = ByteBuffer.allocateDirect(tableVertexWithTriangles.length * BYTES_PER_FLOAT)
                 .order(ByteOrder.nativeOrder())
@@ -69,11 +73,16 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
             ShaderHelper.validateProgram(program);
         }
         GLES20.glUseProgram(program);
-        uColorLocation = GLES20.glGetUniformLocation(program, U_COLOR);
         aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
+        aColorLocation = GLES20.glGetAttribLocation(program, A_COLOR);
+
         vertexData.position(0);
-        GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT, false, 0, vertexData);
+        GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT, false, STRIDE, vertexData);
         GLES20.glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        GLES20.glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GLES20.GL_FLOAT, false, STRIDE, vertexData);
+        GLES20.glEnableVertexAttribArray(aColorLocation);
     }
 
     @Override
@@ -85,16 +94,12 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
         // draw table
-        GLES20.glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6);
         // draw lines
-        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2);
         // draw mallet blue
-        GLES20.glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 1);
         // draw mallet red
-        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_POINTS, 9, 1);
     }
 }
